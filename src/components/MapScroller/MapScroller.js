9b17ce577d6c68f0056ba480d883a24e10620b42
Joshua Byrd <phocks@gmail.com>
2018-05-08 11:17:54 +1000

    Dirty fix for IE and Edge

--- src/components/MapScroller/MapScroller.js
+++ src/components/MapScroller/MapScroller.js
@@ -223,7 +223,7 @@ class MapScroller extends React.Component {
     const canvasEl = document.querySelector("." + styles.stage);
 
     // Auto-convert canvas to Retina display and High DPI monitor scaling
-    canvasDpiScaler(canvasEl, context);
+      canvasDpiScaler(canvasEl, context);
 
     // Build a path generator for our orthographic projection
     path = d3Geo
@@ -765,12 +765,16 @@ class MapScroller extends React.Component {
       context.stroke();
     });
 
-    // context.beginPath();
-    // context.globalAlpha = 1;
-    // context.strokeStyle = "rgba(100, 100, 100, 0.6)";
-    // context.lineWidth = 1.1;
-    // path(australiaOutline);
-    // context.stroke();
+    // TODO: fix Australia stroke outline so it works on IE and Edge retina
+    if (!detectIE()) {
+    // Render the outline of Australia
+    context.beginPath();
+    context.globalAlpha = 1;
+    context.strokeStyle = "rgba(100, 100, 100, 0.6)";
+    context.lineWidth = 1.1;
+    path(australiaOutline);
+    context.stroke();
+    }
   }
 
   componentWillUpdate() {}
@@ -901,6 +905,50 @@ if (!Array.prototype.find) {
       return undefined;
     }
   });
+}
+
+/**
+ * detect IE
+ * returns version of IE or false, if browser is not Internet Explorer
+ */
+function detectIE() {
+  var ua = window.navigator.userAgent;
+
+  // Test values; Uncomment to check result …
+
+  // IE 10
+  // ua = 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)';
+
+  // IE 11
+  // ua = 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko';
+
+  // Edge 12 (Spartan)
+  // ua = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36 Edge/12.0';
+
+  // Edge 13
+  // ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586';
+
+  var msie = ua.indexOf("MSIE ");
+  if (msie > 0) {
+    // IE 10 or older => return version number
+    return parseInt(ua.substring(msie + 5, ua.indexOf(".", msie)), 10);
+  }
+
+  var trident = ua.indexOf("Trident/");
+  if (trident > 0) {
+    // IE 11 => return version number
+    var rv = ua.indexOf("rv:");
+    return parseInt(ua.substring(rv + 3, ua.indexOf(".", rv)), 10);
+  }
+
+  var edge = ua.indexOf("Edge/");
+  if (edge > 0) {
+    // Edge (IE 12+) => return version number
+    return parseInt(ua.substring(edge + 5, ua.indexOf(".", edge)), 10);
+  }
+
+  // other browser
+  return false;
 }
 
 module.exports = MapScroller;
