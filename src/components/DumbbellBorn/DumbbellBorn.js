c46e27cabbec51bfe0cc5f5d4b8473a2285f4dfc
Joshua Byrd <phocks@gmail.com>
2018-06-07 16:23:26 +1000

    Spacing between Dumbbell portals

--- src/components/DumbbellBorn/DumbbellBorn.js
+++ src/components/DumbbellBorn/DumbbellBorn.js
@@ -1,14 +1,15 @@
-const React = require('react');
+const React = require("react");
 const ReactDOM = require("react-dom");
 
-const styles = require('./DumbbellBorn.scss');
+const styles = require("./DumbbellBorn.scss");
 
 class DumbbellBorn extends React.Component {
   render() {
     return ReactDOM.createPortal(
       <div className={styles.wrapper}>
         {this.props.children}
-      </div>, document.querySelector(".dumbbellborn")
+      </div>,
+      document.querySelector(".dumbbellborn")
     );
   }
 }
