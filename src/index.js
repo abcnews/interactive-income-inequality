95a539d7cd8e564db2faa172c6f48aece5e5d5ff
Joshua Byrd <phocks@gmail.com>
2018-08-17 16:39:54 +1000

    Try using the gemini package

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const { render } = require("react-dom");
 const xhr = require("xhr");
 const spanify = require("spanify");
 
+import * as gemini from "gemini";
+
 require("react-select/dist/react-select.css");
 
 require("./lib/modernizr.js"); // Detect browser features
@@ -35,85 +37,89 @@ function init() {
 
   const App = require("./components/App/App");
 
-  render(
-    <App
-      projectName={PROJECT_NAME}
-    />,
-    root
-  );
+  render(<App projectName={PROJECT_NAME} />, root);
 }
 
-// Set up a twin dynamic content article
-const getSupplementaryCMID = () => {
-  const SUPPLEMENTARY_CMID_META_SELECTOR = 'meta[name="supplementary"]';
-  const metaEl = document.querySelector(SUPPLEMENTARY_CMID_META_SELECTOR);
+// // Set up a twin dynamic content article
+// const getSupplementaryCMID = () => {
+//   const SUPPLEMENTARY_CMID_META_SELECTOR = 'meta[name="supplementary"]';
+//   const metaEl = document.querySelector(SUPPLEMENTARY_CMID_META_SELECTOR);
 
-  if (!metaEl) {
-    throw new Error(`${SUPPLEMENTARY_CMID_META_SELECTOR} does not exist`);
-  }
+//   if (!metaEl) {
+//     throw new Error(`${SUPPLEMENTARY_CMID_META_SELECTOR} does not exist`);
+//   }
 
-  let cmid = metaEl.getAttribute("content");
+//   let cmid = metaEl.getAttribute("content");
 
-  if (cmid.indexOf("CMArticle") > -1) {
-    cmid = cmid.match(/id=(\d+)/)[1];
-  }
+//   if (cmid.indexOf("CMArticle") > -1) {
+//     cmid = cmid.match(/id=(\d+)/)[1];
+//   }
 
-  if (cmid != +cmid) {
-    throw new Error(`"${cmid}" does not look like a CMID`);
-  }
+//   if (cmid != +cmid) {
+//     throw new Error(`"${cmid}" does not look like a CMID`);
+//   }
 
-  return cmid;
-};
+//   return cmid;
+// };
 
-// Fetch another CoreMedia article and parse it for dynamic use
-xhr({ url: "/news/" + getSupplementaryCMID() }, (err, response, body) => {
-  function transform() {
-    const doc = new DOMParser().parseFromString(body, "text/html");
-    const startNode = doc.querySelector('a[name="content"]');
-    const endNode = doc.querySelector('a[name="endcontent"]');
+// // Fetch another CoreMedia article and parse it for dynamic use
+// xhr({ url: "/news/" + getSupplementaryCMID() }, (err, response, body) => {
+//   function transform() {
+//     const doc = new DOMParser().parseFromString(body, "text/html");
+//     const startNode = doc.querySelector('a[name="content"]');
+//     const endNode = doc.querySelector('a[name="endcontent"]');
 
-    if (!startNode || !endNode) {
-      console.error(new Error("No content bookends found in document."));
+//     if (!startNode || !endNode) {
+//       console.error(new Error("No content bookends found in document."));
 
-      return init();
-    }
+//       return init();
+//     }
 
-    let currentNode = startNode;
-    const injectionRoot = document.querySelector('[name="fullscript"]');
+//     let currentNode = startNode;
+//     const injectionRoot = document.querySelector('[name="fullscript"]');
 
-    let fetchedNodes = [];
+//     let fetchedNodes = [];
 
-    while (
-      ((currentNode = currentNode.nextSibling),
-      currentNode && currentNode !== endNode)
-    ) {
-      fetchedNodes.push(currentNode);
-    }
+//     while (
+//       ((currentNode = currentNode.nextSibling),
+//       currentNode && currentNode !== endNode)
+//     ) {
+//       fetchedNodes.push(currentNode);
+//     }
 
-    fetchedNodes.forEach(node => {
-      // Use Odyssey API to re-apply smart quotes
-      window.__ODYSSEY__.utils.misc.smartquotes(node);
+//     fetchedNodes.forEach(node => {
+//       // Use Odyssey API to re-apply smart quotes
+//       window.__ODYSSEY__.utils.misc.smartquotes(node);
 
-      // Append fetched content
-      injectionRoot.appendChild(node);
-    });
+//       // Append fetched content
+//       injectionRoot.appendChild(node);
+//     });
 
-    // Unwraps injected content from parent
-    var parent = injectionRoot.parentNode;
-    while (injectionRoot.firstChild)
-    parent.insertBefore(injectionRoot.firstChild, injectionRoot);
-    parent.removeChild(injectionRoot);
+//     // Unwraps injected content from parent
+//     var parent = injectionRoot.parentNode;
+//     while (injectionRoot.firstChild)
+//       parent.insertBefore(injectionRoot.firstChild, injectionRoot);
+//     parent.removeChild(injectionRoot);
 
-    init();
-  }
+//     init();
+//   }
 
-  // Wait for Odyssey
-  if (window.__ODYSSEY__) {
-    transform();
-  } else {
-    window.addEventListener("odyssey:api", transform);
-  }
-});
+//   // Wait for Odyssey
+//   if (window.__ODYSSEY__) {
+//     transform();
+//   } else {
+//     window.addEventListener("odyssey:api", transform);
+//   }
+// });
+
+// Wait for Odyssey
+if (window.__ODYSSEY__) {
+  gemini.fullReplace(init);
+} else {
+  window.addEventListener("odyssey:api", () => {
+    gemini.fullReplace(init);
+  });
+}
 
 if (module.hot) {
   module.hot.accept("./components/App/App", () => {
